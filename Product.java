ad647e1d1a11cdef3eb0b4016a779fd549419e96
Nat Tessema <natnaeltessema1@gmail.com>
2025-04-25 10:46:02 -0700

    Populated product class

---
+++ Product.java
@@ -0,0 +1,49 @@
+public class Product {
+
+    String sku;
+    String productName;
+    double price;
+    String department;
+
+    //Constructor
+    Product(String sku, String productName, double price, String department){
+        this.sku = sku;
+        this.productName = productName;
+        this.price = price;
+        this.department = department;
+    }
+
+    //Getters
+    public String getSku() {
+        return sku;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    //Setters
+    public void setSku(String sku) {
+        this.sku = sku;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+}
