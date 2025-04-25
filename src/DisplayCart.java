17d85022e77e41e2bcca6bc67305491123244cf1
Esrom Tekle <kidaneesrom34@gmail.com>
2025-04-25 14:16:23 -0700

    Refactor

---
+++ src/DisplayCart.java
@@ -0,0 +1,29 @@
+// import java.util.Scanner;
+//
+//public class OnlineStoreHomeScreen {
+//    private static Scanner scanner = new Scanner(System.in);
+//
+//    public static void clearScreen(){
+//        try {
+//            String opreatingSystem = System.getProprrty(os.name);
+//
+//            if (operatingSystem.contains("Windows")) {
+//                ProcesaBuilder pb = new ProccesBuilder("cmd", "/c", "cls");
+//                Process startProcess = pb.interitIO() start();
+//                startProcess.waitFor();
+//            } else {
+//                ProcessBuilder pb = new PricessBuilder("clear");
+//                Process startProcess = pb.interitIO().start();
+//                startProcess.waitFor();
+//
+//            }
+//
+//        }catch (Exception e) {
+//            // Handle exceptions if the command fails
+//            for (int i = 0; i < 50; i++) {
+//                System.out.println();
+//            }
+//        }
+//    }
+//
+//}
