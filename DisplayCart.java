4b85ea8aed983f564d8dbe4bebd436cdc9be78fa
Esrom Tekle <kidaneesrom34@gmail.com>
2025-04-25 11:37:50 -0700

    Commit

--- DisplayCart.java
+++ DisplayCart.java
@@ -1,3 +1,29 @@
  import java.until.Scanner;
 
-public class onlinestore
+public class OnlineStoreHomeScreen {
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static void clearScreen(){
+        try {
+            string opreatingSystem = System.getProprrty(os.name);
+
+            if (operatingSystem.contains("Windows")) {
+                ProcesaBuilder pb = new ProccesBuilder("cmd", "/c", "cls");
+                Process startProcess = pb.interitIO() start();
+                startProcess.waitFor();
+            } else {
+                ProcessBuilder pb = new PricessBuilder("clear");
+                Process startProcess = pb.interitIO().start();
+                startProcess.waitFor();
+
+            }
+
+        }catch (Exception e) {
+            // Handle exceptions if the command fails
+            for (int i = 0; i < 50; i++) {
+                System.out.println();
+            }
+        }
+    }
+
+}
